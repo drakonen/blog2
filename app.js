8a062b5323b52619ec780fd98d3a93f8520416dd
Johan Otten <drakonen@drakonen.com>
2016-10-04 14:56:53 +0200

    Working on adding create post. Updated eslintrc

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.locals.pretty = true;
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,9 +28,9 @@ app.use('/api/1', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,23 +38,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    console.log("Starting in development mode");
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,26 @@ router.get('/posts', function(req, res, next) {
 
     models.Post.find({}, function (err, posts) {
 
-        res.send('respond with a resource' + JSON.stringify([posts]));
+        res.send(JSON.stringify(posts));
     });
 
+});
+
+// new post
+router.post('/post', function(req, res, next) {
+    console.log("req", req.body);
+
+    var data = {
+        title: req.body.data,
+        body: req.body.body,
+        author: req.body.author
+    };
+
+
+    var post = models.Post(data);
+    post.save();
+
+    res.send("ok");
 });
 
 module.exports = router;
