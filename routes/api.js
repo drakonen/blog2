af6d01c1c94ecad1fc6e8d8ccb1913725f43fcee
Johan Otten <drakonen@drakonen.com>
2016-10-04 12:27:20 +0200

    Connect to mongoDB on startup

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var mongoose = require('mongoose');
+var models = require('../models');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/posts', function(req, res, next) {
 
+    models.Post.find({}, function (err, posts) {
 
-  res.send('respond with a resource');
+        res.send('respond with a resource' + JSON.stringify([posts]));
+    });
+
 });
 
 module.exports = router;
