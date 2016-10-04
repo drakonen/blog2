bf32f5d640266bf95940dec54e50a77b6bf230b8
Johan Otten <drakonen@drakonen.com>
2016-10-04 12:26:24 +0200

    Made some rudimentary models

--- models.js
+++ models.js
@@ -1,1 +1,18 @@
 var mongoose = require('mongoose');
+
+var PostSchema = mongoose.Schema({
+    title: String,
+    body: String,
+    author: String // FIXME: link to user
+});
+
+var UserSchema = mongoose.Schema({
+    name: String
+});
+
+
+
+var Post = mongoose.model('Post', PostSchema);
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {Post, User};
