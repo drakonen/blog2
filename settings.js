a8b34fc8d03b4409f0cd6d2dba36c06d8d38498f
Johan Otten <drakonen@drakonen.com>
2016-10-05 13:02:23 +0200

    Added more dev deps, added extra startup commands

--- settings.js
+++ settings.js
@@ -1,5 +1,12 @@
-module.exports = {
+var settings = {
     mongodb: {
         url: "mongodb://localhost/blog2"
     }
 };
+
+
+if (process.env.NODE_ENV == "test") {
+    settings.mongodb.url = "mongodb://localhost/blog2-test"
+}
+
+module.exports = settings;
