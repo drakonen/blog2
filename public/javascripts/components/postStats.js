5aa8f5418fd487fd6fdf02b7f2c33a19129be95f
Johan Otten <drakonen@drakonen.com>
2016-10-11 14:44:43 +0200

    Working on differrent vis styles

--- public/javascripts/components/postStats.js
+++ public/javascripts/components/postStats.js
@@ -72,14 +72,7 @@ class nextLetterer {
             .attr("transform", transform)
     }
 
-    render() {
-
-        this.tree = d3.tree()
-            .size([360, 170]); // rotation (not the full 360) and radius
-
-        let treeData = d3.hierarchy(createTree(this.data));
-        this.tree(treeData);
-
+    getMinMaxFromTree(treeData) {
         let min = Infinity;
         let max = -Infinity;
 
@@ -87,42 +80,87 @@ class nextLetterer {
             min = d3.min([min, node.data.count]);
             max = d3.max([max, node.data.count]);
         });
-        console.log("min", min, max)
+
+        return [min, max];
+
+    }
+
+    render() {
+        let styles = {
+            linear: {
+                project: function (x, y) {
+                    return [x, y];
+                },
+                tree: d3.tree()
+                    .size([this.width, this.height]),
+                render: function (selection) {
+
+                }
+
+            },
+
+            radial: {
+                project: function (x, y) {
+                    var angle = x / 180 * Math.PI, radius = y;
+                    return [radius * Math.cos(angle), radius * Math.sin(angle)];
+                },
+                tree: d3.tree()
+                    .size([360, 170]), // rotation and radius
+                render: function (selection) {
+                    console.log("selection", selection.size());
+                    selection.attr("transform", function (d) {
+                        console.log("d", d)
+                        return `rotate(${d.x}), translate(${d.y}, 0) rotate(-${d.x})`
+                    })
+                }
+            }
+
+        }
+
+        // let style = "linear";
+        let style = "radial";
+
+        let project = styles[style].project;
+        this.tree = styles[style].tree;
+
+        let treeData = d3.hierarchy(createTree(this.data));
+        this.tree(treeData);
+
+        let [min, max] = this.getMinMaxFromTree(treeData);
+        console.log("min", min, max);
 
         let fontSizeScale = d3.scaleLinear().domain([min, max]).range([12, 20]);
 
 
 
-        function project(x, y) {
-            var angle = x / 180 * Math.PI, radius = y;
-            return [radius * Math.cos(angle), radius * Math.sin(angle)];
-        }
 
-        let links = treeData.links();
-        this.canvas.selectAll(".link").data(links).enter()
-            .append("path")
-            .attr("class", "link")
-            .attr("d", function(d) {
-                return "M" + project(d.source.x, d.source.y)
-                    + "C" + project(d.source.x, (d.source.y + d.target.y) / 2)
-                    + " " + project(d.target.x, (d.source.y + d.target.y) / 2)
-                    + " " + project(d.target.x, d.target.y);
-            })
-            .attr("stroke", "#ccc")
-            .attr("stroke-width", "1")
-            .attr("fill", "none")
+        let links = this.canvas.selectAll(".link").data(treeData.links());
 
-        treeData.each((node) => {
-            this.canvas.append("text")
-                .text(node.data.name)
-                .attr("transform", function () {
-                    return `rotate(${node.x}), translate(${node.y}, 0) rotate(-${node.x})`
+        links
+            .enter()
+                .append("path")
+                .attr("class", "link")
+            .merge(links)
+                .attr("d", function(d) {
+                    return "M" + project(d.source.x, d.source.y)
+                        + "C" + project(d.source.x, (d.source.y + d.target.y) / 2)
+                        + " " + project(d.target.x, (d.source.y + d.target.y) / 2)
+                        + " " + project(d.target.x, d.target.y);
                 })
-                .attr("font-size", function () {
-                    return fontSizeScale(node.data.count);
+                .attr("stroke", "#ccc")
+                .attr("stroke-width", "1")
+                .attr("fill", "none")
+
+        let nodes = this.canvas.selectAll("text").data(treeData.descendants());
+
+        nodes.enter()
+            .append("text")
+                .text(function (d) { return d.data.name; })
+                .attr("font-size", function (d) {
+                    return fontSizeScale(d.data.count);
                 })
+                .call(styles[style].render)
                 .attr("text-anchor", "middle")
-        });
 
 
     }
